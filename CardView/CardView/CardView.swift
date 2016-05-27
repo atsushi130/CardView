243ba924b3f7a4b49f2ddab4d04e10e826a257d2
= <=>
2016-05-27 22:29:27 +0900

    add CardView

---
+++ CardView/CardView/CardView.swift
@@ -0,0 +1,13 @@
+//
+//  CardView.swift
+//  CardView
+//
+//  Created by ATSUSHI on 2016/05/27.
+//  Copyright © 2016年 ATSUSHI. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+}
