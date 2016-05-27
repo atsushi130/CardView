fdd877e3f3c9b823a90121d6c3070ac558567d6b
= <=>
2016-05-27 23:09:29 +0900

    load nib file

--- CardView/CardView/CardView.swift
+++ CardView/CardView/CardView.swift
@@ -8,6 +8,35 @@
 
 import UIKit
 
+private let NibName = "CardView"
+
 class CardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    func setup() {
+        
+        for view in self.subviews {
+            view.removeFromSuperview()
+        }
+        
+        let view = self.factory()
+        view.frame = self.bounds
+        self.addSubview(view)
+        
+    }
+    
+    func factory() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        return UINib(nibName: NibName, bundle: bundle).instantiateWithOwner(self, options: nil).first as! UIView
+    }
     
 }
